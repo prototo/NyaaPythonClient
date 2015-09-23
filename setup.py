cf863d3c7e4642d0dee3c7b668679bd950f1e91c
Greg Mackelden <gmackelden@gmail.com>
2015-09-23 19:10:58 +0200

    Updated package configuration

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 
-setup(name='NyaPythonClient',
-    version='0.1',
+setup(name='nyaa_python_client',
+    version='0.0.0',
+    license='MIT',
     py_modules=['nyaa']
 )
