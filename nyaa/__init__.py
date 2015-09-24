e0395305c282872e0f925db672d2ea2318971d8d
Greg Mackelden <gmackelden@gmail.com>
2015-09-24 14:20:04 +0200

    Updates package configuration

--- nyaa/__init__.py
+++
@@ -1,2 +0,0 @@
-from nyaa.client import NyaaClient
-
--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='nyaa_python_client',
     version='0.0.0',
     license='MIT',
-    py_modules=['nyaa']
+    py_modules=['nyaa_client']
 )
